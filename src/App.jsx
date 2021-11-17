6c0e35644f98ecf50b5556c68810dbe7014e8920
Lucas Mateus <85304236+thedevbr@users.noreply.github.com>
2021-11-17 20:54:28 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import './index.css';
+
+function App() {
+  const [value, setValue] = useState(32);
+  let novovalor = Math.round(((value - 32) * 5) / 9);
+  return (
+    <div
+      style={{
+        boxSizing: "border-box",
+        background: "#f0f0f0",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+    
+      <section
+        style={{
+          borderRadius: ".5rem",
+          background: "#Fff",
+          color: "black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "4rem",
+          gap: "1rem",
+          boxShadow: "0 0 .2rem .1rem rgba(0, 0, 0,.1)",
+        }}
+      >
+      <h1 style={{color:"#710"}}>Convert Fahrenheit for Celsius</h1>
+        <h1>Fahrenheit</h1>
+        <input
+          name="inputOne"
+          type="number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          style={{
+            width: "100%",
+            outline: "none",
+            border: "transparent",
+            boxShadow: "0 0 .20rem 0 rgba(0, 0, 0, 0.7)",
+            padding: ".8rem",
+            color: novovalor < 0 ? "red" : "green",
+          }}
+        />
+        <h2>for</h2>
+        <h1 style={{ color: "black" }}>Celsius</h1>
+        <h2
+          style={{
+            textAlign: "center",
+            color: novovalor < 0 ? "red" : "green",
+          }}
+        >
+          {novovalor}º
+        </h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
